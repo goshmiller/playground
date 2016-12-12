93bd069ae2fcb8bc3411cf07a48510ba58fba4f1
goshmiller <goshmiller@gmail.com>
2016-12-12 14:19:01 -0700

    Adding some generic anonymous functions.

--- program.js
+++ program.js
@@ -1,1 +1,28 @@
 document.writeln('Hello, world!');
+
+//This is an Immediately Invoked Function (IIFy)
+(function () {
+console.log('Executed!');
+})();
+
+
+//Pluralsight Rapid JavaScript Training - Functions
+//https://app.pluralsight.com/player?course=rapid-javascript-training&author=mark-zamoyta&name=rapid-javascript-training-m7&clip=6&mode=live
+var app = {};
+
+(function (ns) {
+    ns.name = 'Josh';
+})(app);
+console.log(app.name);
+
+
+//Trying more
+var app2 = {};
+var jQuery = {};
+
+(function (ns, $) {
+    ns.name = 'Josh';
+    console.log($ === jQuery);
+    console.log(jQuery);
+})(app2, jQuery);
+
